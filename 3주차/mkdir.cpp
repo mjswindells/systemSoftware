9a507114a66e684a71aee5bb84aba97ce5bb53f4
mjswindells <wgs06052@naver.com>
2022-09-15 18:14:16 -0700

    3주차

--- 3주차/mkdir.cpp
+++ 3주차/mkdir.cpp
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #define PERMS 0755
 #define MAX_PATH_LEN 1024
 
 int main(int argc, char const *argv[]) {
+
     if (argc != 2) {
         printf("Usage: %s [pathname] n ", argv[0]);
-        perror("args error");
         exit(-1);
     }
+
     if (mkdir(argv[1], PERMS) == -1) {
         perror("mkdir() error!");
         exit(-2);
     }
+
     return 0;
 }
