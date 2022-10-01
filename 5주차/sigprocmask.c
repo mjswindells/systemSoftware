1e06351e90e29026c18da34e5c7b9620e7d5909b
mjswindells <wgs06052@naver.com>
2022-10-01 10:42:41 -0700

    수정

--- 5주차/sigprocmask.c
+++ 5주차/sigprocmask.c
@@ -5,13 +5,13 @@
 void mySigHandler(int signum);
 
 int main(int argc, char const *argv[]) {
-    int cnt = 10;
+    int cnt = 100;
     sigset_t set;
     sigemptyset(&set);
     sigaddset(&set, SIGUSR1);
     // 시그널을 블록상태로 설정하거나 해제가능
-    // unblock 시키거나 해당 시그널에 대한 action을 ignore로 변경전가지 실행이
-    // 안됨
+    // unblock 시키거나 해당 시그널에 대한
+    // action을 ignore로 변경전까지 실행이 안됨
     sigprocmask(SIG_BLOCK, &set, NULL); // SIGUSR1 시그널을 블록
     signal(SIGUSR1, mySigHandler);
     printf("My PID is : %d\n", getpid());
